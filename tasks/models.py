8ff4ed9b862930edf472747fea4efcfc4e448722
Emerson Fan <metalraccoon@gmail.com>
2025-02-23 15:45:29 +0800

    feat: Add task related tables

--- tasks/models.py
+++ tasks/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from core.models import Timestamp
 from django.contrib.postgres.fields import ArrayField
+from users.models import CustomUser
+from django.utils import timezone
 
 
 class RecursionRule(models.Model):
-
     class Frequency(models.TextChoices):
         DAILY = "daily", "Daily"
         WEEKLY = "weekly", "Weekly"
@@ -18,14 +19,7 @@ class RecursionRule(models.Model):
         db_table = "tasks_recursion_rule"
 
 
-# 　this should be changed see claud
-class Task(models.Model, Timestamp):
-    class Status(models.TextChoices):
-        PENDING = "pending", "Pending"
-        COMPLETED = "completed", "Completed"
-        IN_PROGRESS = "in_progress", "In progress"
-        CANCELLED = "cancelled", "Cancelled"
-
+class TaskTemplate(Timestamp):
     class Priority(models.IntegerChoices):
         LOW = 1, "Low"
         MEDIUM = 2, "Medium"
@@ -33,15 +27,43 @@ class Task(models.Model, Timestamp):
         URGENT = 4, "Urgent"
         CRITICAL = 5, "Critical"
 
-    # how due_date works for weekly or monthly task?
-    due_date = models.DateTimeField(null=False, blank=True)
     request_body = models.JSONField()
+    created_user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     assigned_users = ArrayField(models.IntegerField(), null=True, blank=True)
-    location = models.CharField(max_length=256, null=True, blank=True)
+    location = models.CharField(max_length=255, null=True, blank=True)
     recursion_rule = models.ForeignKey(
         RecursionRule, on_delete=models.SET_NULL, null=True, blank=True
+    )
+    priority = models.IntegerField(choices=Priority.choices, default=Priority.MEDIUM)
+
+    def soft_delete(self):
+        self.deleted_at = timezone.now()
+        self.save()
+
+    class Meta:
+        db_table = "tasks_task_template"
+
+
+class TaskInstance(Timestamp):
+    class Status(models.TextChoices):
+        PENDING = "pending", "Pending"
+        COMPLETED = "completed", "Completed"
+        IN_PROGRESS = "in_progress", "In progress"
+        CANCELLED = "cancelled", "Cancelled"
+
+    due_date = models.DateTimeField(null=True, blank=True)
+    finished_time = models.DateTimeField(null=True, blank=True)
+    template = models.ForeignKey(
+        TaskTemplate, on_delete=models.PROTECT, related_name="instances"
     )
     task_status = models.CharField(
         max_length=20, choices=Status.choices, default=Status.PENDING
     )
-    priority = models.IntegerField(choices=Priority.choices, default=Priority.MEDIUM)
+
+    class Meta:
+        db_table = "tasks_task_instance"
+        indexes = [
+            models.Index(fields=["template", "created_at"]),
+            models.Index(fields=["task_status", "created_at"]),
+            models.Index(fields=["due_date"]),
+        ]
