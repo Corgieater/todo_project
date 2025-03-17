824f28574e9b3c43c89b86ff4135018d0628dad3
Emerson Fan <metalraccoon@gmail.com>
2025-03-17 14:31:46 +0800

    test: Add user creation tests

--- users/tests.py
+++ users/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from users.models import CustomUser
 
-# Create your tests here.
+
+class TestCustomUser(TestCase):
+    def test_create_valid_user(self):
+        user = CustomUser.objects.create_superuser(
+            email="user@example.com",
+            name="user",
+            password="abcd4321",
+        )
+
+        self.assertIsNotNone(user)
+        self.assertEqual(user.email, "user@example.com")
+        self.assertTrue(user.check_password("abcd4321"))
+
+    def test_create_user_without_email_error(self):
+        with self.assertRaises(ValueError):
+            CustomUser.objects.create_user(email=None, password="test123")
+
+    def test_create_superuser(self):
+        admin_user = CustomUser.objects.create_superuser(
+            email="admin@example.com", name="Admin User", password="adminpassword123"
+        )
+        self.assertTrue(admin_user.is_superuser)
+        self.assertTrue(admin_user.is_staff)
